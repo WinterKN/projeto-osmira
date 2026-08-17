7100854dee515698d074425cfa6a64fc864e868a
AlgPS <alvarolgk112021@gmail.com>
2026-08-17 18:37:40 -0300

    interact scene e script
    Adicionado um script para interação e um objeto cena base para todos outros objetos de interação *EM TESTE

---
+++ scripts/interactable/interaction.gd
@@ -0,0 +1,20 @@
+class_name interactable extends Node
+@export var interaction_ui: Node
+
+
+@export var player: Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_trigger_body_entered(body: Node3D) -> void:
+	interaction_ui.alternar_visibilidade(true)
+
+func _on_trigger_body_exited(body: Node3D) -> void:
+	print("hello")

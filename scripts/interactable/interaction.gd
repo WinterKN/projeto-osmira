5a32be613b3aeb1362d0bceb8a0d11733658222a
Alvinho-osmira <alvaro.nobre@aluno.ce.gov.br>
2026-08-20 10:51:18 -0300

    Interaction working

--- scripts/interactable/interaction.gd
+++ scripts/interactable/interaction.gd
@@ -4,17 +4,25 @@ class_name interactable extends Node
 
 
 @export var player: Player
+var ui_esta_visivel: bool = false
 
-
+	
+func _unhandled_input(event: InputEvent) -> void:
+	# Só permite interagir com a tecla "E" (ação "interact") se a UI estiver visível
+	if event.is_action_pressed("interact") and ui_esta_visivel:
+		print("Interagiu com o objeto!")
+		queue_free()
+		# Coloque aqui a lógica do que acontece quando o jogador interage
 	
 func _on_trigger_body_entered(body: Node3D) -> void:
 	print("enter")
-	
+
 # --- BUSCA A UI DE INTERAÇÃO PELO GRUPO ---
 	if interaction_ui != null:
 			print("oi")
 			interaction_ui.set_text("[E] INTERAGIR")
 			interaction_ui.alternar_visibilidade(true)
+			ui_esta_visivel = true # Atualiza o estado para visível
 
 	else:
 			push_error("ERRO: UI de interação não foi encontrada no grupo 'ui_interacao'!")
@@ -25,10 +33,12 @@ func _on_trigger_body_entered(body: Node3D) -> void:
 			print("ao")
 			interaction_ui = uis[0]
 			interaction_ui.alternar_visibilidade(false)
+			ui_esta_visivel = false # Atualiza o estado para invisível
 			# Se encontrou a UI, executa o código normalmente
 
 
 func _on_trigger_body_exited(body: Node3D) -> void:
 	print("leave")
-	interaction_ui.alternar_visibilidade(false)
-	
+	if interaction_ui != null:
+		interaction_ui.alternar_visibilidade(false)
+	ui_esta_visivel = false # Atualiza o estado para invisível

5a1397c237a1e1270bd762b306d1a3dd3536dfb2
Alvinho-osmira <alvaro.nobre@aluno.ce.gov.br>
2026-08-20 10:12:11 -0300

    Interaction_UI fixed

--- scripts/interactable/interaction.gd
+++ scripts/interactable/interaction.gd
@@ -11,6 +11,13 @@ func _on_trigger_body_entered(body: Node3D) -> void:
 	print("enter")
 	
 # --- BUSCA A UI DE INTERAÇÃO PELO GRUPO ---
+	if interaction_ui != null:
+			print("oi")
+			interaction_ui.set_text("[E] INTERAGIR")
+			interaction_ui.alternar_visibilidade(true)
+
+	else:
+			push_error("ERRO: UI de interação não foi encontrada no grupo 'ui_interacao'!")
 	if interaction_ui == null:
 
 		var uis = get_tree().get_nodes_in_group("ui_interacao")
@@ -19,23 +26,9 @@ func _on_trigger_body_entered(body: Node3D) -> void:
 			interaction_ui = uis[0]
 			interaction_ui.alternar_visibilidade(false)
 			# Se encontrou a UI, executa o código normalmente
-	if interaction_ui != null:
-			print("oi")
-			interaction_ui.set_text("[E] INTERAGIR")
-			interaction_ui.alternar_visibilidade(true)
 
-	else:
-			push_error("ERRO: UI de interação não foi encontrada no grupo 'ui_interacao'!")
 
 func _on_trigger_body_exited(body: Node3D) -> void:
 	print("leave")
 	interaction_ui.alternar_visibilidade(false)
 	
-
-
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
-
-
-func _on_area_3d_body_exited(body: Node3D) -> void:
-	pass # Replace with function body.

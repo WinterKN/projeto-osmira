9ec91df3a5dd0eecc4dabafde2fa157d223082cf
AlgPS <alvarolgk112021@gmail.com>
2026-08-29 23:13:03 -0300

    Added quests types (missing completion)

--- Game_manager.gd
+++ Game_manager.gd
@@ -1,10 +1,41 @@
 extends Node
 
-signal quest_changed
+signal quest_changed()
+signal area_entered(area: AREA)
+signal person_talked(person: PERSON)
 
-enum  QUEST_TYPE {GATHER}
+enum  QUEST_TYPE {ENTER, TALK_WITH}
+enum PERSON {BEN, ANNA}
+enum AREA {OCA_PRINCIPAL}
 var activeQuests: Array[Quest] = []
 
 func add_quest(quest:Quest):
 	activeQuests.append(quest.duplicate())
+	quest_changed.emit()
+
+func enter_area(area: AREA, quantity):	
+	for quest: Quest in activeQuests:
+		if quest.questType == QUEST_TYPE.ENTER and quest.questArea == area:
+			quest.quantityCollected += quantity
+			
+	quest_changed.emit()
+	area_entered.emit()
+
+func talk_with(person: PERSON):
+	for quest: Quest in activeQuests:
+		if quest.questType == QUEST_TYPE.TALK_WITH and quest.questPerson == person:
+			quest.quantityCollected += 1
+	
+	quest_changed.emit()
+	person_talked.emit()
+
+func own_quest_done(person:PERSON):
+	for quest: Quest in activeQuests:
+		var questDone =  quest.quantityCollected >= quest.quantityGoal
+		
+		if quest.to == person and questDone:
+			return quest
+			
+func quest_done(quest:Quest):
+	activeQuests.erase(quest)
 	quest_changed.emit()
---
+++ scripts/quests/area_quest.gd
@@ -0,0 +1,22 @@
+extends StaticBody3D
+
+@export var area: GameManager.AREA
+
+var inRange
+
+func _input(_event: InputEvent) -> void:
+	if inRange:
+		_enter()
+		
+func _enter():
+	GameManager.enter_area(area, 1)
+	#queue_free()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		inRange = true
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		inRange = false
